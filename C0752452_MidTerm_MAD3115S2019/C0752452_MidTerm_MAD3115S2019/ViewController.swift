f022506827c35671e05c1a97126161bfa956d473
hargunmarya <hargunmarya2726@gmail.com>
2019-07-11 18:56:23 -0400

    [Hargun]created new project

---
+++ C0752452_MidTerm_MAD3115S2019/C0752452_MidTerm_MAD3115S2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0752452_MidTerm_MAD3115S2019
+//
+//  Created by Hargun Marya on 2019-07-11.
+//  Copyright © 2019 Hargun Marya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
