9db9e325e6114e7eb0dd219a7100d3563fbc8027
hargunmarya <hargunmarya2726@gmail.com>
2019-07-12 16:07:43 -0400

    [Hargun] created classes for customer

---
+++ C0752452_MidTerm_MAD3115S2019/C0752452_MidTerm_MAD3115S2019/classes/Bill.swift
@@ -0,0 +1,9 @@
+//
+//  Bill.swift
+//  C0752452_MidTerm_MAD3115S2019
+//
+//  Created by Hargun Marya on 2019-07-12.
+//  Copyright © 2019 Hargun Marya. All rights reserved.
+//
+
+import Foundation
