ac5980dfeb7fe0606e3e154839d834a0e3103675
hargunmarya <hargunmarya2726@gmail.com>
2019-07-12 18:32:13 -0400

    [Hargun]  created show details class for showing the details

---
+++ C0752452_MidTerm_MAD3115S2019/C0752452_MidTerm_MAD3115S2019/ShowDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DhowDetailsViewController.swift
+//  C0752452_MidTerm_MAD3115S2019
+//
+//  Created by Hargun Marya on 2019-07-12.
+//  Copyright © 2019 Hargun Marya. All rights reserved.
+//
+
+import UIKit
+
+class ShowDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
